9d84d498543edb269c4e316e9bc7b5e8f1d7c938
Arun Jose <12997751+ArunJose@users.noreply.github.com>
2021-01-13 01:11:53 +0530

    Add image and description for park

--- seeds/index.js
+++ seeds/index.js
@@ -29,6 +29,10 @@ const seedDB = async () => {
           parkTypes[randomIdx(parkTypes)]
         }`,
         location: `${cities[randomIdx(cities)].city}`,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+        price: Math.floor(Math.random() * 7) * 50,
+        image: `https://source.unsplash.com/collection/208422`,
       });
       await newPark.save();
     }
